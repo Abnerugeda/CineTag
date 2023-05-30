31b42f31505346c7a10dc022a47f3c8aa939a5b1
Abner Ugeda <devabnerugeda@gmail.com>
2023-05-30 13:53:03 -0300

    é o gato bonifacio

--- src/pages/Initial/index.jsx
+++ src/pages/Initial/index.jsx
@@ -1,5 +1,6 @@
 import Banner from 'components/Banner'
 import Cabecalho from 'components/Cabecalho'
+import Card from 'components/Card'
 import Rodape from 'components/Rodape'
 import Titulo from 'components/Titulo'
 import React from 'react'
@@ -12,6 +13,7 @@ export default function Initial() {
       <Titulo>
         <h1>Um lugar para guardar seus vídeos e filmes!</h1>
       </Titulo>
+      <Card id='1' titulo='Gato bonifácio' capa='https://thecatapi.com/api/images/get?format-src&type=png' />
       <Rodape/>
     </>
   )
