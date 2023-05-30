363f80598b6857db1823ec804857818d8c0ebec2
Abner Ugeda <devabnerugeda@gmail.com>
2023-05-30 11:57:35 -0300

    Banner finalizado

--- src/pages/Initial/index.jsx
+++ src/pages/Initial/index.jsx
@@ -1,3 +1,4 @@
+import Banner from 'components/Banner'
 import Cabecalho from 'components/Cabecalho'
 import Rodape from 'components/Rodape'
 import React from 'react'
@@ -6,6 +7,7 @@ export default function Initial() {
   return (
     <>
       <Cabecalho/>
+      <Banner imagem="home"/>
       <Rodape/>
     </>
   )
